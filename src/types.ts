07a6cf504842a0179377320b2d6cc10155175476
Doris <157232491+DorisLiu1101@users.noreply.github.com>
2026-01-21 14:59:25 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -34,7 +34,6 @@ export interface GameState {
   currentRevealIndex: number;
   scenario: Scenario | null;
   selectedCards: (CardData | null)[];
-  badGoosePlayerId: number | null;
   distractorCards: CardData[];
 }
 
